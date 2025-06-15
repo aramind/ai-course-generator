61bd02f54ca8c216d211a62daaa030bb16e6e5d5
aramind <88814622+aramind@users.noreply.github.com>
2025-06-16 00:37:34 +0800

    first commit

--- ai-course-generator/app/index.tsx
+++ ai-course-generator/app/index.tsx
@@ -1,5 +1,5 @@
 import Colors from "@/constants/Colors";
-import { Image, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -14,8 +14,40 @@ export default function Index() {
         style={{
           height: 400,
           width: "100%",
+          marginTop: 70,
         }}
       />
+      <View
+        style={{
+          padding: 25,
+          backgroundColor: Colors.PRIMARY,
+          height: "100%",
+          borderTopLeftRadius: 35,
+          borderTopRightRadius: 35,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            textAlign: "center",
+            color: Colors.WHITE,
+          }}
+        >
+          Welcome to Coaching Guru
+        </Text>
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+            textAlign: "center",
+            color: Colors.WHITE,
+          }}
+        >
+          Transform your ideas into engaging educational content, effortlessly
+          with AI!
+        </Text>
+      </View>
     </View>
   );
 }
--- ai-course-generator/constants/Colors.jsx
+++ ai-course-generator/constants/Colors.jsx
@@ -1,4 +1,5 @@
 export default {
   WHITE: "#fff",
   BLACK: "#333",
+  PRIMARY: "#007",
 };
